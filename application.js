53d54bced0416a2d6467e90c820a81d3b7320c81
Eunjung Ahn <Eve@Eve.local>
2016-06-27 18:53:35 -0700

    working with jquery, response color depending on guess

--- application.js
+++ application.js
@@ -1,31 +1,62 @@
 var player = {
-  bankroll: 100
+  bankroll: 100,
+  right_guesses: 0, 
+  wrong_gueses: 0
 };
-
+var bet;
+var guess; 
 function randomNumGen(min, max) {
-    return Math.floor(Math.random() * (max - min + 1) + min);
+  return Math.floor(Math.random() * (max - min + 1) + min);
 }
-
-var keepPlaying = true;
-
-while (keepPlaying) {
-  var bet = prompt('Place your bets(between $5 and $10 please)!'); //TODO: do a check to make sure they don't bet an amount greater than their bankroll.
-  var guess = prompt('Now guess a number between 1 and 10!');
+ 
+function generateAndCheckGuess(guess) {
   var answer = randomNumGen(1,10);
-
   if (guess === answer) {
-     player.bankroll += bet;
-     alert("You are correct! You won $" + bet + "! You now have $" + player.bankroll + ".");
-  } else if (guess > (answer - 1) && guess < (answer + 1)) { 
-    alert("The answer was " + answer +  "! You were off by just 1! You will still retain your previous balance of $" + player.bankroll + ".");  
+    console.log('Correct ' + answer);
+    player.bankroll += bet;     
+    $('#results').html("<p id='correct-guess'>Correct!</p>");
+  } else if (guess === (answer + 1) || guess === (answer - 1)) {
+    console.log('Close ' + answer);
+    $('#results').html("<p id='close-guess'>Close! It was actually " + answer +"</p>");
   } else {
-    player.bankroll -= bet;
-    alert("Wrong! The answer was " + answer + "! You lost $" + bet + "! Your balance is now $" + player.bankroll + ".");
-  }
-  if (player.bankroll <= 0) {
-    alert("You're bankrupt!");
-    keepPlaying = false;
-  } else {
-  keepPlaying = confirm('Make another bet?');
+    console.log('Wrong! ' + answer);
+    player.bankroll -= bet;     
+    $('#results').html("<p id='incorrect-guess'>Incorrect! It was actually " + answer + "</p>");
   }
 }
+
+
+$(document).ready(function() {
+  $('#player-stats #bank-roll').html(player.bankroll);
+
+  $("#submit-button").on('click', function(e) {
+    e.preventDefault();
+    bet = $("input[name='bet']").val();
+    guess = $("input[name='guess']").val();
+    generateAndCheckGuess(guess);
+    console.log('You now have $' + player.bankroll);
+    $('#player-stats #bank-roll').html(player.bankroll);
+  });
+
+//var keepPlaying = true;
+//  while (keepPlaying) {
+//    var bet = prompt('Place your bets(between $5 and $10 please)!'); //TODO: do a check to make sure they don't bet an amount greater than their bankroll.
+//    var guess = prompt('Now guess a number between 1 and 10!');
+//    var answer = randomNumGen(1,10);
+
+//    if (guess === answer) {
+//       player.bankroll += bet;
+//       alert("You are correct! You won $" + bet + "! You now have $" + player.bankroll + ".");
+//    } else if (guess > (answer - 1) && guess < (answer + 1)) { 
+//      alert("The answer was " + answer +  "! You were off by just 1! You will still retain your previous balance of $" + player.bankroll + ".");  
+//    } else {
+//      player.bankroll -= bet;
+//      alert("Wrong! The answer was " + answer + "! You lost $" + bet + "! Your balance is now $" + player.bankroll + ".");
+//    }
+//    if (player.bankroll <= 0) {
+//      alert("You're bankrupt!");
+//      keepPlaying = false;
+//    } else {
+//    keepPlaying = confirm('Make another bet?');
+//    }
+});
